fa34c32755e51e0039991546c382445abb00818d
Arno Temmerman <arno.temmer@gmail.com>
2023-03-17 17:00:41 +0100

    [WPO3] oefening 1 (start)

---
+++ H10 - Graaftraversals/WPO 3/Vraag1-Tests.rkt
@@ -0,0 +1,70 @@
+#lang r7rs
+
+;; Algoritmen en datastructuren II
+;; Vraag 1 - Ouder-kind-relaties in DFT- & BFT-bossen
+(import (scheme base)
+        (scheme write)
+        (a-d graph unweighted config)
+        (except (a-d graph-traversing dft-unweighted) 
+                root-nop node-nop edge-nop)
+        (a-d graph-traversing bft)
+        (a-d graph examples undirected-unweighted))
+
+;; a) DFT versie (met parent-list):
+
+(define (dft-parents g)
+  ;; Ga diepte eerst door de graaf
+  ;; en print voor elke knoop het pad
+  ;; van die knoop tot de wortel
+  (define parent-stack '())
+  (dft g
+       root-nop ;; root-discovered
+       node-nop ;; node-discovered
+       node-nop ;; node-processed
+       edge-nop ;; edge-discovered
+       edge-nop ;; edge-processed
+       edge-nop ;; edge-bumped
+       ;; (roots)
+       ))
+
+;;  Tests:
+(display "connected met DFT:")(newline)
+(display "------------------")(newline)
+(dft-parents connected)(newline)
+
+(display "three-cc met DFT:")(newline)
+(display "-----------------")(newline)
+(dft-parents three-cc)(newline)
+
+(display "kite met DFT:")(newline)
+(display "-------------")(newline)
+(dft-parents kite)(newline)
+
+;; b) BFT versie (met parent-vector):
+
+(define (bft-parents g)
+  ;; Ga breedte eerste door de graaf
+  ;; en print voor elke knoop het pad 
+  ;; van die knoop tot de wortel
+  (define parent-vector (make-vector (order g) #f))
+
+  (bft g
+       root-nop ;; root-discovered
+       node-nop ;; node-discovered
+       edge-nop ;; edge-discovered
+       edge-nop ;; edge-bumped
+       ;; (roots)
+       ))
+
+;;  Tests:
+(display "connected met BFT:")(newline)
+(display "------------------")(newline)
+(bft-parents connected)(newline)
+
+(display "three-cc met BFT:")(newline)
+(display "-----------------")(newline)
+(bft-parents three-cc)(newline)
+
+(display "kite met BFT:")(newline)
+(display "-------------")(newline)
+(bft-parents kite)(newline)

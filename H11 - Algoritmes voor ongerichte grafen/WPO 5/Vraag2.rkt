3e4efde28e543ac3483f00a75b8349f9996eef34
Arno Temmerman <arno.temmer@gmail.com>
2023-04-06 11:06:48 +0200

    [WPO5] oefening 2 (oplossing)

--- H11 - Algoritmes voor ongerichte grafen/WPO 5/Vraag2.rkt
+++ H11 - Algoritmes voor ongerichte grafen/WPO 5/Vraag2.rkt
@@ -32,7 +32,11 @@
   (define node-sets (dset:new (order g)))
   (quicksort edges (lambda (edge1 edge2) (< (cadr edge1) (cadr edge2))))
   (do ((edge-idx 0 (+ edge-idx 1)))
-      ((= edge-idx n-e) forest)
+      ((= edge-idx n-e)
+       (map (lambda (edge)
+              (cons (dset:find node-sets (car edge))
+                    edge))
+            forest))
     (let* ((edge (vector-ref edges edge-idx))
 	   (from (car edge))
 	   (weight (cadr edge))
@@ -43,6 +47,12 @@
         (set! forest (cons (list from weight to) forest))
         (dset:union! node-sets from-set to-set)))))
 
+;;Uitleg: Eens het algoritme klaar is komen de sets in node-sets overeen
+;;        met de samenhangende componenten van de graaf en dus
+;;        ook met de minimale spanningsbomen (elke boom in het bos beslaat
+;;        immers een van de samenhangende component van de graf).
+;;        Daarom kunnen we de set-identiteiten (resultaat van dset:find)
+;;        gebruiken als identificatie voor de minimale spannings bomen.
 
 ;;Test Vraag 2
 (display "MST van graf cormen571split, gevonden door algoritme van Kruskal: ")(newline)
@@ -54,4 +64,22 @@
 ;;{{ID FROM WEIGHT TO} ...}
 ;;{{5 3 9 4} {7 0 8 7} {5 2 7 3} {7 6 6 8} {7 0 4 1} {5 2 4 5} {7 6 1 7}}
 
-
+;; b) Worst-case performantie: 
+;;     - dset:find: O(log(|V|))
+;;     - dset:find wordt door map opgeroepen voor elke edge in het
+;;       Minimale Spannings Bos (MSF), in de worst case is dat elke
+;;       edge van de graaf, dus we krijgen: O(|E|*log(|V|))
+;;     - Kruskal using Quicksort: O(|E|*log(|V|))
+;;    TOTAAL: O(|E|*log(|V|) + |E|*log(|V|)) = O(2*|E|*log(|V|)) = O(|E|*log(|V|))
+;;    --> Worst-case performantie is dezelfde als bij de originele implementatie
+;;
+;; Extra:
+;;    Geamortiseerde performantie:
+;;     - dset:find: O(α(|V|)
+;;     - dset:find wordt door map opgeroepen voor elke edge in het
+;;       Minimale Spannings Bos (MSF), in de worst case is dat elke
+;;       edge van de graaf, dus we krijgen: O(|E|*α(|V|))
+;;     - Kruskal using Quicksort: O(|E|*log(|V|))
+;;    TOTAAL: O(|E|*log(|V|) + |E|*α(|V|)) = O(|E| * (log(|V|) +  α(|V|)))  = O(|E|*log(|V|))
+;;            (want α(|V|) is een veel trager stijgende functie dan log(|V|))
+;;    --> Geamortiseerde performantie is dezelfde als bij de originele implementatie

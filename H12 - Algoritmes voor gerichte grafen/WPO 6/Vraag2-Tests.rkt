1024388a4ee12e8eef7dcf93b753b6cdaa30b7f0
Arno Temmerman <arno.temmer@gmail.com>
2023-04-21 17:36:57 +0200

    [WPO6] oefening 2

--- H12 - Algoritmes voor gerichte grafen/WPO 6/Vraag2-Tests.rkt
+++ H12 - Algoritmes voor gerichte grafen/WPO 6/Vraag2-Tests.rkt
@@ -20,13 +20,14 @@
   (define highest-back-edge (make-vector (order g) -1))
   (define sc-components (make-vector (order g) -1))
   (define nr-of-components 0)
-  (define included (make-vector (order g) #f)) ; node-indexed vector die bijhoudt of een knoop reeds in een component zit
-  (define stack (stack:new)) ; stack om huidig afgelegd pad bij te houden
+  (define sv (make-vector (order g) '())) ; stack om huidig afgelegd pad bij te houden
+  (define top '())
   (dft
    g
    root-nop
    (lambda (node)                                       ; node-discovered
-     (stack:push! stack node)                           ; log het afgelegd pad op de stack
+     (vector-set! sv node top)
+     (set! top node)
      (vector-set! preorder-numbers node preorder-time)  ; deel preorder-nummers uit
      (vector-set! highest-back-edge node preorder-time) ; tijdens het afdalen is de node (voorlopig) zelf het kleinste preorder-nummer dat we kunnen bereiken
      (set! preorder-time (+ preorder-time 1)))
@@ -35,19 +36,20 @@
               (vector-ref preorder-numbers node))    ; als kleinste preorder-nummer ==> We hebben een "afgewerkt" component gevonden
        (set! nr-of-components (+ 1 nr-of-components))
        (let loop
-         ((t (stack:pop! stack)))                       ; pop het pad dat werd afgelegd SINDS het discover'en van de node  
+         ((t top))                       ; pop het pad dat werd afgelegd SINDS het discover'en van de node  
+         (set! top (vector-ref sv t))
          (vector-set! sc-components t nr-of-components) ; iedere knoop op dat pad behoort tot het sterk samenhangend component
-         (vector-set! included t #t)                    ; registreer dat de knoop reeds tot een component behoort om kruisbogen te filteren
+         (vector-set! sv t 'included)                    ; registreer dat de knoop reeds tot een component behoort om kruisbogen te filteren
          (unless (eq? t node)                           ; we blijven dit doen tot het hele pad tot en met de node van de stack is gehaald
-           (loop (stack:pop! stack))))))
+           (loop top)))))
    edge-nop          ; edge-discovered
    (lambda (from to) ; edge-processed
-     (if (not (vector-ref included to))
+     (if (not (eq? 'included (vector-ref sv to)))
          (vector-set! highest-back-edge
                       from (min (vector-ref highest-back-edge from)
                                 (vector-ref highest-back-edge to)))))
    (lambda (from to)                    ; edge-bumped 
-     (if (not (vector-ref included to)) ; avoid cross-edges!!
+     (if (not (eq? 'included (vector-ref sv to))) ; avoid cross-edges!!
          (vector-set! highest-back-edge
                       from (min (vector-ref highest-back-edge from)
                                 (vector-ref preorder-numbers to))))))
